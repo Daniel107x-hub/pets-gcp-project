2be89370f3cb7206a7ca62f36571d4aeb2ed7aa5
daniel107x-hub <DMejiaV310796@outlook.com>
2025-06-11 21:14:07 -0600

    Initialize next project with svg support, axios and form validation

---
+++ frontend/src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+type Props = {}
+
+const Error = (props: Props) => {
+  return (
+    <div>Something went wrong :S</div>
+  )
+}
+
+export default Error
---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+type Props = {}
+
+const NotFound = (props: Props) => {
+  return (
+    <div>Page not found</div>
+  )
+}
+
+export default NotFound
