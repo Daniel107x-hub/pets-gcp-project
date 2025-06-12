656fcdbd420f7a0c29b343963839a24c13688b84
Daniel107x-hub <DMejiaV310796@outlook.com>
2025-06-12 11:23:10 -0600

    Added sqlite config in startup and migrations

--- Api/Pets.Api/Program.cs
+++ Api/Pets.Api/Program.cs
@@ -6,9 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddIdentityApiEndpoints<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddAuthorization();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("AppDb"));
-// Add services to the container.
 
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("PetsDbSqlite")));
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
