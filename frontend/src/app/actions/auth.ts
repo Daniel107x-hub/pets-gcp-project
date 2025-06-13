39e98a6d5355683c922d4729c57a52f52d2eee75
daniel107x-hub <DMejiaV310796@outlook.com>
2025-06-12 21:38:23 -0600

    Added sign in

--- frontend/src/app/actions/auth.ts
+++ frontend/src/app/actions/auth.ts
@@ -1,5 +1,6 @@
 import { FormState, LoginFormSchema } from "@/app/lib/definitions";
 import axiosInstance from "@/app/config/axiosConfig";
+import { redirect } from "next/navigation";
 
 export async function login(state: FormState, formData: FormData){
     const validateFields = LoginFormSchema.safeParse({
@@ -23,6 +24,30 @@ export async function login(state: FormState, formData: FormData){
             message: "Something went wrong"
         }
     }
-    console.log("Successfully logged in");
-    // Create user session and redirect user
+    redirect("/home");
+}
+
+export async function signIn(state: FormState, formData: FormData){
+    const validateFields = LoginFormSchema.safeParse({
+        email: formData.get("email"),
+        password: formData.get("password")
+    });
+    if(!validateFields.success){
+        return {
+            errors: validateFields.error.flatten().fieldErrors
+        }
+    }
+    // Execute api call
+    const { email, password } = validateFields.data;
+    try{
+        const response = await axiosInstance.post('/register', {
+            email,
+            password
+        });
+    }catch(e: any){
+        return {
+            message: "Something went wrong"
+        }
+    }
+    redirect("/");
 }
--- frontend/src/app/sign-in/page.tsx
+++ frontend/src/app/sign-in/page.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+"use client"
+import React, { useActionState } from 'react'
+import Input from '../_components/input/Input'
+import { signIn } from '../actions/auth';
+import Link from 'next/link';
 
 type Props = {}
 
-const page = (props: Props) => {
+const SignIn = (props: Props) => {
+  const [state, action, pending] = useActionState(signIn, undefined);
+  
   return (
-    <div>Sign in</div>
+    <section className='h-screen items-center flex justify-center'>
+      <form className="flex flex-col bg-amber-100 gap-4 p-8 rounded-lg items-center" action={action}>
+        <div className="flex flex-col w-full">
+          <Input type="email" name="email" label="Email"/>
+          {
+            state?.errors?.email && <p className="text-accent text-xs">{state.errors.email}</p>
+          }
+        </div>
+        <div className="flex flex-col w-full">
+          <Input type="password" name="password" label="Password"/>
+          {
+            state?.errors?.password && (
+              <div className="w-full text-accent text-xs">
+                <p>Password must:</p>
+                <ul className="list-disc">
+                  { 
+                    state.errors.password.map((error) => <li key={error}>{error}</li>)
+                  }
+                </ul>
+              </div>
+            )
+          }
+        </div>
+        <button className="bg-accent px-10 py-2 rounded-2xl text-xl font-extrabold hover:cursor-pointer hover:brightness-95" type="submit" disabled={pending}>Sign In</button>
+        <Link href={"/"} className="text-accent text-sm font-semibold">Already a user?</Link>
+      </form>
+    </section>
   )
 }
 
-export default page
+export default SignIn;
